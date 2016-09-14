d552438a964c28a3be10a9474fff94220eb2978f
ninayang91 <na.yang@hotmail.com>
2016-09-14 19:51:54 -0400

    add comments

--- Main.java
+++ Main.java
@@ -6,10 +6,14 @@ public class Main {
 
 	}
 	
+	//Given two 32-bit numbers M and N; and two bit positions i and j;
+	//insert M into N such that M starts at bit j and ends at bit i
+	//bits j through i have enough space to fit all of M
 	public static int updateBits(int N, int M, int i, int j){
-		int left = ~((1<<(j+1))-1);
-	    int right = (1<<i)-1;
-		int mask=left|right;
+		//Suppose i=2, j=4, we need a mask like 11100011
+		int left = ~((1<<(j+1))-1);//111100000, (j+1)0s
+	    int right = (1<<i)-1;//0000000011, i 1s
+		int mask=left|right;//11100011
 		return (mask&N)|(M<<i);
 	}
 
